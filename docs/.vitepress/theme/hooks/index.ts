8aea84bc42ea6219c549b6c6a211517ce3fd6986
yuyi <1138866572@qq.com>
2025-09-08 23:25:22 +0800

    废弃动画清理

--- docs/.vitepress/theme/hooks/index.ts
+++ docs/.vitepress/theme/hooks/index.ts
@@ -1,4 +1,2 @@
 export { useHomeViewTransition } from './useHomeViewTransition.ts'
-export { useMeteorAnimation } from './useMeteorAnimation.ts'
-export { useSakuraAnimation } from './useSakuraAnimation.ts'
 export { useToggleAppearance } from './useToggleAppearance.ts'
