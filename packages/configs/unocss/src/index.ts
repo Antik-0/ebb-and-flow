815bc931a96ece93b1f6c898880c037741f73d6b
yuyi <1138866572@qq.com>
2025-09-28 00:08:20 +0800

    unocss 主题配置

--- packages/configs/unocss/src/index.ts
+++ packages/configs/unocss/src/index.ts
@@ -15,6 +15,23 @@ export const presetBase = definePreset(() => {
         }
       })
     ],
+    theme: {
+      colors: {
+        // 正文用 neutral
+        // accent 用 gray
+        // muted 用 zinc
+        accent: '#6a6a71',
+        muted: '#98989f',
+        brand: {
+          DEFAULT: '#5ac6d3',
+          1: '#5ac6d3',
+          2: '#a8b1ff',
+          3: '#5c73e7'
+        },
+        border: '#3c3f44',
+        divider: '#2e2e32'
+      }
+    },
     shortcuts: [
       ['flex-col', 'flex flex-col'],
       ['flex-center', 'items-center justify-center']
