a58bbbc9b026f8813bdd22e6646d055767ea8278
yuyi <1138866572@qq.com>
2025-12-21 21:43:33 +0800

    chore: lint 检查

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs/promises'
 import { join } from 'node:path'
-import { fileURLToPath, URL } from 'node:url'
+import { URL, fileURLToPath } from 'node:url'
 import ora from 'ora'
 import pc from 'picocolors'
 import rootPackageJSON from '../package.json' with { type: 'json' }
