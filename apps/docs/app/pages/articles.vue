f9d1e29271db177e8fc5f68f696dbeaca34e2239
yuyi <gpt-antik@proton.me>
2026-03-01 21:18:20 +0800

    refactor: docs

--- apps/docs/app/pages/articles.vue
+++ apps/docs/app/pages/articles.vue
@@ -1,87 +1,39 @@
 <script setup lang='ts'>
-import { EllipsisVertical } from 'ebb-theme/icons'
-import { motion } from 'motion-v'
-
-definePageMeta({ layout: 'page' })
+import '../assets/article.css'
+import { random } from '@repo/utils'
 
 useSeoMeta({ title: formatTitle('归档') })
 
 const { data } = await useFetch('/api/articles')
 
-const articles = computed(() => {
-  if (!data.value) return []
-  return [...Object.entries(data.value)]
-})
+const articles = data.value
 </script>
 
 <template>
-  <div class="px-8">
-    <div class="mx-auto max-w-200">
-      <motion.h1
-        :animate="{ opacity: [0, 1], y: [40, 0] }"
-        class="mb-4 text-center"
-        :transition="{ duration: 0.8 }"
-      >
-        <span class="text-2xl text-teal tracking-2 font-600">文章归档</span>
-      </motion.h1>
-
-      <div
-        v-for="[year, items] in articles"
-        :key="year"
-        class="my-4"
-      >
-        <motion.h2
-          :animate="{ opacity: [0, 1], x: [60, 0] }"
-          class="py-2"
-          :transition="{ duration: 0.8 }"
-        >
-          <span class="text-xl text-accent-foreground tracking-1">{{ year }}</span>
-          <span class="text-sm text-muted-foreground ml-4">{{ items.length }}篇文章</span>
-        </motion.h2>
-
-        <ul class="py-2 flow-root">
-          <motion.li
-            v-for="(item, index) in items"
-            :key="index"
-            :animate="{
-              opacity: [0, 1],
-              y: [40, -20, 0]
-            }"
-            class="group py-2"
-            :custom="index"
-            :transition="{ ease: 'easeInOut', duration: 1, delay: index * 0.1 }"
-          >
-            <NuxtLink class="text-brand-2/60 flex gap-2 items-center relative" :to="item.path">
-              <NuxtTime
-                class="text-xs text-muted-foreground text-right w-12"
-                :datetime="item.lastUpdated"
-                day="2-digit"
-                month="2-digit"
-              />
-
-              <EllipsisVertical class="text-zinc bottom-full left-14 absolute -ml-px" />
-              <span class="mx-1 rounded-1 bg-zinc size-1.5 transition-height group-hover:(bg-brand-3 h-4)"></span>
-
-              <span class="flex-1 truncate transition-transform group-hover:text-brand-3 group-hover:translate-x-2">
+  <div class="article-page">
+    <div id="container" class="cards-container">
+      <ul class="cards">
+        <li v-for="(item, index) in articles" :key="index">
+          <NuxtLink class="card" :to="item.path">
+            <img alt="img" :src="item.cover" />
+            <div class="px-2 py-1 bg-black">
+              <p class="text-cyan-50 leading-6 font-bold mb-2 text-center truncate">
                 {{ item.title }}
-              </span>
-
-              <span
-                v-if="item.tags"
-                class="inline-flex gap-3 items-center"
-              >
-                <span
-                  v-for="(tag, index) in item.tags"
-                  :key="index"
-                  class="text-sm text-muted-foreground"
-                >
-                  #{{ tag }}
+              </p>
+              <p class="text-xs flex gap-2 items-center">
+                <span v-for="(tag, idx) in item.tags" :key="idx">
+                  {{ '#' + tag }}
                 </span>
-              </span>
-            </NuxtLink>
-          </motion.li>
-        </ul>
-      </div>
+                <span class="flex-1"></span>
+                <NuxtTime
+                  class="text-xs text-muted-foreground"
+                  :datetime="item.lastUpdated"
+                />
+              </p>
+            </div>
+          </NuxtLink>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
--- apps/docs/server/api/articles.ts
+++ apps/docs/server/api/articles.ts
@@ -1,9 +1,9 @@
-import { groupBy } from '@repo/utils'
+import { groupBy, random } from '@repo/utils'
 
 interface Article {
   title: string
   path: string
-  tags: string[]
+  tags?: string[]
   lastUpdated: string
 }
 
@@ -14,9 +14,30 @@ export default defineEventHandler(async event => {
     .order('lastUpdated', 'DESC')
     .all()
 
-  const articles = groupBy<Article>(data, item =>
-    new Date(item.lastUpdated).getFullYear().toString()
-  )
+  const covers = [
+    '/wallhaven-rrwvdq.webp',
+    '/cover/wallhaven-6lm9w6.jpg',
+    '/cover/wallhaven-9dwldk.jpg',
+    '/cover/wallhaven-awd13a.jpg',
+    '/cover/wallhaven-eaw661z.jpg',
+    '/cover/wallhaven-jeeypy.jpg',
+    '/cover/wallhaven-qrr12q.jpg',
+    '/cover/wallhaven-xe761z.jpg',
+    '/cover/wallhaven-xezkro.jpg'
+  ]
+  const length = covers.length
 
-  return articles
+  const articles = groupBy(data, item =>
+    new Date(item.lastUpdated!).getFullYear().toString()
+  ) as unknown as Record<string, Article[]>
+
+  const res = Object.values(articles)
+    .flat()
+    .map(item => {
+      const index = random(0, length - 1)
+      const cover = covers[index]
+      return { cover, ...item }
+    })
+
+  return res
 })
